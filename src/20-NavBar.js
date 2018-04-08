5e75f8ee1346541c5a3d1c2bc96562fa1487b2e5
Charles-Antoine LEGER <toutane.leger@gmail.com>
2018-04-08 21:18:16 +0200

    Adding NavBar

--- src/20-NavBar.js
+++ src/20-NavBar.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import { Button, InputGroupButton, Input, InputGroupAddon, InputGroup, Collapse,
-  Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Navbar, NavbarToggler, NavbarBrand,
-  Nav, NavItem, NavLink, Media, CardImg, Container } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import {
+  Button,
+  InputGroupButton,
+  Input,
+  InputGroupAddon,
+  InputGroup,
+  Collapse,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Media,
+  CardImg,
+  Container,
+  UncontrolledDropdown
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { getLogout, getUser } from "./api/BeAPI";
 
-import users from './database/users.js'
+import users from "./database/users.js";
 
 export default class NavBar extends React.Component {
   constructor(props) {
@@ -12,14 +32,35 @@ export default class NavBar extends React.Component {
 
     this.toggle = this.toggle.bind(this);
     this.state = {
+      logged: false,
+      currentUser: [],
       isOpen: false
     };
     this.dropdownToggle = this.dropdownToggle.bind(this);
-    this.stateDropdown= {
+    this.logoutFunction = this.logoutFunction.bind(this);
+
+    this.stateDropdown = {
       dropdownOpen: false
     };
-
   }
+
+  componentDidMount() {
+    getUser().then(user =>
+      this.setState({
+        currentUser: user
+      }, user.error === undefined
+          ? (this.setState({logged: true}))
+          : (this.setState({logged: false}))
+    )
+    );
+  }
+
+  logoutFunction() {
+    getLogout().then(response => this.setState({
+      logged: false
+    }));
+  }
+
   dropdownToggle() {
     this.setState({
       dropdownOpen: !this.state.dropdownOpen
@@ -33,36 +74,81 @@ export default class NavBar extends React.Component {
   }
   render() {
     return (
-      <Navbar color="faded" light toggleable>
-      <Container>
-      <NavbarToggler right onClick={this.toggle} />
-      <NavbarBrand tag={Link} to="/logout"><i className='far fa-clipboard fa-lg text-primary'/>&nbsp; To do </NavbarBrand>
-      <Collapse isOpen={this.state.isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-        <NavItem><NavLink tag={Link} to="/messages"><i className="far fa-bell"/></NavLink></NavItem>
-        {/* {users.map(user =>
-        <div className="d-flex justify-content-end">
-        <NavItem><CardImg width="5%" src={user.avatar} alt={user.username} /></NavItem>
-        </div>)} */}
-        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.dropdownToggle}>
-        {users.map((user, i) =>
-        <DropdownToggle nav caret key={i}>
-          {user.username}
-        </DropdownToggle>)}
-        <DropdownMenu right>
-        {users.map((user, i) => 
-        <DropdownItem header key={i}>Signed in as <b>{user.full_name}</b></DropdownItem>)}
-        <DropdownItem divider />
-          <DropdownItem tag={Link} to="/settings">Your Profile</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem tag={Link} to="/settings">Settings</DropdownItem>
-          <DropdownItem>Sign out</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-        </Nav>
-      </Collapse>
-      </Container>
-    </Navbar>
-    )
+      <div>
+        <Navbar className="navbar-dark bg-dark" toggleable>
+          <Container>
+            <NavbarToggler right onClick={this.toggle} />
+            <NavbarBrand tag={Link} to="/home">
+              <i className="far fa-clipboard fa-lg" />&nbsp; to do-<span className="text-primary">
+                app
+              </span>
+            </NavbarBrand>
+            {this.state.logged
+            ? (<Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink tag={Link} to="/settings">
+                      <i className="far fa-bell fa-lg mt-2" />
+                    </NavLink>
+                  </NavItem>
+                  <Dropdown
+                    isOpen={this.state.dropdownOpen}
+                    toggle={this.dropdownToggle}
+                  >
+                    {this.state.currentUser.map((user, i) => (
+                      <div key={i}>
+                        <UncontrolledDropdown nav inNavbar>
+                          <DropdownToggle nav caret key={i}>
+                            <img
+                              src={user.avatar}
+                              className="rounded"
+                              height="30"
+                              alt="user-name"
+                            />
+                          </DropdownToggle>
+                          <DropdownMenu right>
+                            <DropdownItem header key={i}>
+                              Signed in as{" "}
+                              <b className="text-white"> {user.username}</b>
+                            </DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem tag={Link} to="/settings">
+                              Your Profile
+                            </DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem tag={Link} to="/settings">
+                              <i className="fa fa-cog" /> Settings
+                            </DropdownItem>
+                            <DropdownItem
+                              onClick={this.logoutFunction}
+                              tag={Link}
+                              to="/home"
+                            >
+                              <i className="fas fa-sign-out-alt" /> Logout
+                            </DropdownItem>
+                          </DropdownMenu>
+                        </UncontrolledDropdown>
+                      </div>
+                    ))}
+                  </Dropdown>
+                </Nav>
+              </Collapse>
+            ) : (
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavLink className="mt-2" tag={Link} to="/login">
+                    <b>Login</b>
+                  </NavLink>
+                  <NavLink className="text-muted mt-2">or</NavLink>
+                  <NavLink className="mt-2" tag={Link} to="/signup">
+                    <b>Sign up</b>
+                  </NavLink>
+                </Nav>
+              </Collapse>
+            )}
+          </Container>
+        </Navbar>
+      </div>
+    );
   }
 }
