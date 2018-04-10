e768307afadcf2e43facb27d62830b8e8b7cfa02
Charles-Antoine LEGER <toutane.leger@gmail.com>
2018-04-10 16:38:33 +0200

    Error fix

--- src/10.1-LogInMenu.js
+++ src/10.1-LogInMenu.js
@@ -3,7 +3,7 @@ import { Button, Jumbotron, Col, Row, Container, Card, CardTitle, CardText, Card
 import { Link } from 'react-router-dom';
 
 import Menu from './10.3-Menu';
-import NavBar from './20-NavBar';
+import NavBar from './02-NavBar';
 import Settings from './views/04-Settings';
 
 const LogInMenu = (props) => {
