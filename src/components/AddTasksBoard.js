344a97200c442163fda445988214330d20518ce5
Charles-Antoine LEGER <toutane.leger@gmail.com>
2018-04-18 22:38:07 +0200

    Add tasksBoard component

--- src/components/AddTasksBoard.js
+++ src/components/AddTasksBoard.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Collapse, Card, CardHeader, CardTitle, CardBody } from 'reactstrap';
+import { Collapse, Card, CardHeader, CardTitle, CardBody, Form, FormGroup,
+Label, Input, InputGroup, Button, Row, Col, ButtonGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import DatePicker from "react-datepicker";
+import { InputGroupAddon } from '../utils/InputGroupAddon';
 
 export default class AddTasksBoard extends React.Component {
   constructor(props) {
@@ -15,14 +18,65 @@ export default class AddTasksBoard extends React.Component {
          <Collapse isOpen={this.props.addTasksBoard}>
           <Card outline color="primary">
             <CardHeader>
-              <CardTitle className="mt-2"><i className="fa fa-plus text-info mr-1"/>Add tasks board</CardTitle>
+              <i className="fa fa-plus text-info mr-1"/>Add tasks board
             </CardHeader>
             <CardBody>
-              Anim pariatur cliche reprehenderit,
-              enim eiusmod high life accusamus terry richardson ad squid. Nihil
-              anim keffiyeh helvetica, craft beer labore wes anderson cred
-              nesciunt sapiente ea proident.
-            </CardBody>
+            <Row>
+              <Col>
+                <Form>
+                  <FormGroup>
+                    <Label>Tasks title</Label>
+                    <Input
+                      // onChange={input => this.taskTitleInputFunction(input)}
+                      // value={this.state.tasksTitleInput}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label>Tasks description</Label>
+                    <Input
+                      type="textarea"
+                      // onChange={input => this.taskDescriptionInputFunction(input)}
+                      // value={this.state.tasksDescriptionInput}
+                    />
+                  </FormGroup>
+                  {/* <hr className="my-3"/>
+                  <Button color="info" onClick={this.advancedOptionsCollapse}>
+                    <i className="fa fa-cog">&nbsp;</i>Advanced options
+                  </Button>&nbsp;
+                  <Button color="success" onClick={this.personalizationCollapse}>
+                    <i className="fas fa-paint-brush" />&nbsp;Personalization
+                  </Button> */}
+                </Form>  
+              </Col>
+              <Col md="4">
+                  <div className="d-flex justify-content-center align-items-center">
+                    <FormGroup>
+                      <Label> Tasks date</Label>
+                      <DatePicker
+                        // selected={this.state.selectedDay}
+                        // onChange={this.taskDateInputFunction}
+                        />
+                    </FormGroup>
+                    <Button outline color="primary" className="ml-3 mt-2"><i className="fa fa-list fa-fw"/></Button>
+                  </div>
+                <hr className="my-1"/>
+                {/* <div className="d-flex justify-content-between">
+                <ButtonGroup vertical>
+                  <Button outline color="primary"><i className="fa fa-list fa-fw"/></Button>
+                  <Button className="mt-3" outline color="primary"><i className="fa fa-list fa-fw"/></Button>
+                </ButtonGroup>
+                <ButtonGroup vertical>
+                  <Button outline color="primary"><i className="fa fa-list fa-fw"/></Button>
+                  <Button className="mt-3" outline color="primary"><i className="fa fa-list fa-fw"/></Button>
+                </ButtonGroup>
+                <ButtonGroup vertical>
+                  <Button outline color="primary"><i className="fa fa-list fa-fw"/></Button>
+                  <Button className="mt-3" outline color="primary"><i className="fa fa-list fa-fw"/></Button>
+                </ButtonGroup>
+                </div> */}
+              </Col>
+            </Row>
+          </CardBody>
           </Card>
           <hr className="my-4"/>
         </Collapse>
