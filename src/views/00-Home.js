0eec7b1a74233443c86fbe79d11519f2cecc89df
Benoit LEGER-DERVILLE <benoit.leger@stelau.com>
2018-06-07 11:11:35 +0200

    Home.js conflict

--- src/views/00-Home.js
+++ src/views/00-Home.js
@@ -28,7 +28,7 @@ export default class Home extends React.Component {
   componentDidMount() {
     console.log(this.props.isLogged);
     getProjects().then(resProjects =>
-          this.setState({ projects: resProjects 
+          this.setState({ projects: resProjects
           }, () => resProjects.map(project =>
             getTasks(project.project_id).then(tasks =>
               this.setState({
@@ -37,7 +37,7 @@ export default class Home extends React.Component {
             }
   // (this.props.isLogged)
   //   ? getProjects().then(resProjects =>
-  //       this.setState({ projects: resProjects 
+  //       this.setState({ projects: resProjects
   //       }, () => resProjects.map(project =>
   //         getTasks(project.project_id).then(tasks =>
   //           this.setState({
