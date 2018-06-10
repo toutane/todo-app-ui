288819b0a029e2442fe55f5535bba87c5548fae8
Benoit LEGER-DERVILLE <benoit.leger@stelau.com>
2018-06-10 15:30:50 +0200

    isLogged Routing almost fix

--- src/techComponents/LittleSwitchRouter.js
+++ src/techComponents/LittleSwitchRouter.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
 class LittleSwitchRouter extends Component {
-  
-  render() {
-    return (
-      <div>
 
-      </div>
-    );
-  }
+  render() {
+    console.log(this.props)
+    return React.Children.map(this.props.children, (child, index) => {
+      return (this.props.isLogged == index) ? child : null
+  })
+}
 }
 
 export default LittleSwitchRouter;
