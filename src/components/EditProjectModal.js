26345c26e658f3cf0de2c2a7dc7b9cc4b2a83e02
Charles-Antoine LEGER <toutane.leger@gmail.com>
2018-06-18 23:44:37 +0200

    Delete/Add projectEditModal

---
+++ src/components/EditProjectModal.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import { Input, InputGroup, CardBody, Card, Button, DropdownToggle,
+  DropdownMenu, DropdownItem, ButtonDropdown, ButtonGroup, Collapse, 
+  FormGroup, Modal, ModalHeader, ModalBody, ModalFooter, } from 'reactstrap';
+import { InputGroupAddon } from '../utils/InputGroupAddon';
+import { updateProjects } from "../api/BeAPI";
+
+import { icons } from "../database/icons";
+
+export default class EditProjectModal extends React.Component {
+  constructor(props) {
+  super(props);
+
+  this.dropdownActiveFunction = this.dropdownActiveFunction.bind(this);
+  this.updateProjectFunction = this.updateProjectFunction.bind(this);
+
+  this.state = {
+    dropdownOpen: false,
+    editModal: true,
+    errorName: false,
+    dropSelectItem: "Project icon",
+    dropSelectItemIcon: "",
+    color: "",
+    input: "",
+    style: {}
+  }
+}
+
+  dropdownActiveFunction() {
+    this.setState({dropdownOpen: !this.state.dropdownOpen})
+  }
+
+  onAddProjectInput(e) {
+    this.setState({
+      input: e.target.value,
+      errorName: false
+    });
+  }
+
+  onAddProjectIcon(icon) {
+    this.setState({
+      dropSelectItem: icon.icon_name,
+      dropSelectItemIcon: icon.icon,
+      icon: icon.icon, 
+      color: icon.icon_color,
+      style: icon.style
+    });
+  }
+
+  updateProjectFunction(project) {
+    if (this.state.input !== "") {
+      console.log(project, this.state.icon)
+        updateProjects(project._id, {
+          project_name: this.state.input,
+          project_icon: this.state.icon
+        }).then(x => {
+          this.setState({
+            input: "",
+            icon: "",
+            dropSelectItem: "",
+            dropSelectItemIcon: "",
+            color: "",
+            style: "" 
+          },
+          () => this.props.updateListFunction())
+        })
+    }
+    else {
+      this.setState({errorName: true})
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+         <Modal isOpen={this.props.isOpenModal}>
+          <ModalHeader><i className="fa fa-edit mr-2"/>Edit project</ModalHeader>
+          <ModalBody>
+          <FormGroup>
+              <InputGroup className="d-flex justify-content-between">
+                <InputGroupAddon addonType="prepend">
+                  <i className={this.props.project.project_icon} />
+                </InputGroupAddon>
+                <Input
+                  onChange={e => this.onAddProjectInput(e)}
+                  value={this.state.input}
+                  placeholder={this.props.project.project_name}
+                />
+              </InputGroup>
+              </FormGroup>
+              <ButtonGroup className="d-flex justify-content-between align-items-baseline">
+                <ButtonDropdown
+                
+                  isOpen={this.state.dropdownOpen}
+                  toggle={this.dropdownActiveFunction}
+                >
+                  <DropdownToggle caret outline color="primary">
+                    <i className={this.state.dropSelectItemIcon} style={this.state.style}/>{" "}
+                    {this.state.dropSelectItem}
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    {icons.map((icon, i) => (
+                      <DropdownItem
+                        onClick={() => this.onAddProjectIcon(icon)}
+                        key={i}
+                      >
+                        <i className={icon.icon} style={icon.style}/> {icon.icon_name}
+                      </DropdownItem>
+                    ))}
+                  </DropdownMenu>
+                </ButtonDropdown>
+                {this.state.errorName 
+                  ? <span className="text-danger ml-2">You must enter a name for your project</span>
+                  : null}
+              </ButtonGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="secondary" onClick={() => this.props.changeStateModal()}>Cancel</Button>
+            <Button color="primary" onClick={() => this.updateProjectFunction(this.props.project)}>Update</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    )
+  }
+};
